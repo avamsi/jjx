78a8057f893ae2b9b9aa447cb62a9ca2c834d35c
Vamsi Avula <avamsi07@gmail.com>
2026-01-04 21:22:18 +0530

    simplify hooks processing
    Change-Id: Id0000000dc107f593dabd80a441e1f0cd420946f

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,30 @@
-struct HooksSettings {
-    post_commit: Vec<String>,
-    post_squash: Vec<String>,
-}
-
-impl TryFrom<&jj_lib::settings::UserSettings> for HooksSettings {
-    type Error = jj_lib::config::ConfigGetError;
-
-    fn try_from(settings: &jj_lib::settings::UserSettings) -> Result<Self, Self::Error> {
-        use jj_lib::config::ConfigGetResultExt as _;
-        Ok(Self {
-            post_commit: settings
-                .get("x.hooks.post-commit")
-                .optional()?
-                .unwrap_or_default(),
-            post_squash: settings
-                .get("x.hooks.post-squash")
-                .optional()?
-                .unwrap_or_default(),
-        })
-    }
-}
+use anyhow::Context as _;
 
 fn run_command(cmd: &[String]) -> anyhow::Result<()> {
-    let Some((program, args)) = cmd.split_first() else {
-        return Ok(());
-    };
+    let (program, args) = cmd.split_first().context("no command")?;
     let status = std::process::Command::new(program).args(args).status()?;
     anyhow::ensure!(status.success(), "{program}: {status}");
     Ok(())
 }
 
-fn run_post_op_hooks(h: &jj_cli::cli_util::CommandHelper) -> anyhow::Result<()> {
-    let hooks = HooksSettings::try_from(h.settings())?;
-    match h.matches().subcommand() {
-        Some(("commit", _)) => run_command(&hooks.post_commit),
-        Some(("squash", _)) => run_command(&hooks.post_squash),
-        _ => Ok(()),
-    }
+fn run_post_op_hook(h: &jj_cli::cli_util::CommandHelper) -> anyhow::Result<()> {
+    let key = match h.matches().subcommand() {
+        Some(("commit", _)) => "x.hooks.post-commit",
+        Some(("squash", _)) => "x.hooks.post-squash",
+        _ => return Ok(()),
+    };
+    use jj_lib::config::ConfigGetResultExt as _;
+    h.settings()
+        .get::<Vec<String>>(key)
+        .optional()?
+        .map_or(Ok(()), |hook| run_command(&hook).context(key))
 }
 
 fn main() -> std::process::ExitCode {
     jj_cli::cli_util::CliRunner::init()
         .add_dispatch_hook(|ui, helper, command| {
             command(ui, helper)?;
-            run_post_op_hooks(helper).map_err(jj_cli::command_error::user_error)
+            run_post_op_hook(helper).map_err(jj_cli::command_error::user_error)
         })
         .run()
         .into()
